c23d3eb700499684917663e8bf5262997e202430
Pharia Le <pharia.le@gmail.com>
2020-03-05 00:55:12 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+def reverse_each_word(sentence)
+  words = sentence.split(" ")
+  new_sentence = []
+  
+  words.each_with_index do |word, i|
+    letters = word.split("")
+    new_word = letters.reverse
+    new_sentence << new_word.join
+  end
+  
+  new_sentence.join(" ")
+end
